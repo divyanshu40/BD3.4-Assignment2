f9893b92e8fffa5582af79b9c0745ac8677aae9c
divyanshu40 <137522047+divyanshu40@users.noreply.github.com>
2024-07-10 15:42:12 +0000

    commit changes

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app.listen(PORT, () => {
 });
 app.use(cors());
 let tasks = [
-  { taskId: 1, text: 'Fix bug #101', priority: 2 },
-  { taskId: 2, text: 'Implement feature #202', priority: 1 },
-  { taskId: 3, text: 'Write documentation', priority: 3 }
+  { taskId: 1, text: "Fix bug #101", priority: 2 },
+  { taskId: 2, text: "Implement feature #202", priority: 1 },
+  { taskId: 3, text: "Write documentation", priority: 3 },
 ];
 // Endpoint 1. Add a Task to the Task List
 function addNewTask(tasks, taskId, text, priority) {
@@ -28,67 +28,84 @@ app.get("/tasks/add", (req, res) => {
   res.json({ tasks: result });
 });
 // Endpoint 2. Read All Tasks in the Task List
-function readAllTasks(tasks) {
-  return tasks;
+let updatedTasks = [
+  { taskId: 1, text: "Fix bug #101", priority: 2 },
+  { taskId: 2, text: "Implement feature #202", priority: 1 },
+  { taskId: 3, text: "Write documentation", priority: 3 },
+  { taskId: 4, text: "Review code", priority: 1 },
+];
+function readTasks(updatedTasks) {
+  return updatedTasks;
 }
 app.get("/tasks", (req, res) => {
-  let result = readAllTasks(tasks);
-  res.json({ tasks: result });
+  let tasks = readTasks(updatedTasks);
+  res.json({ tasks: tasks });
 });
-// Endpoint 3. Sort Tasks by Priority
+//Endpoint 3. Sort Tasks by Priority
 function sortTasksByPriority(task1, task2) {
   return task1.priority - task2.priority;
 }
 app.get("/tasks/sort-by-priority", (req, res) => {
-  let tasksCopy = tasks.slice();
+  let tasksCopy = updatedTasks.slice();
   tasksCopy.sort(sortTasksByPriority);
-  res.json({tasks: tasksCopy });
+  res.json({ tasks: tasksCopy });
 });
 // Endpoint 4. Edit Task Priority
-function editPriorityById(tasks, taskId, priority) {
-  for (let i = 0; i < tasks.length; i++) {
-    if (tasks[i].taskId === taskId) {
-      tasks[i].priority = priority;
+function editPriorityById(updatedTasks, taskId, priority) {
+  for (let i = 0; i < updatedTasks.length; i++) {
+    if (updatedTasks[i].taskId === taskId) {
+      updatedTasks[i].priority = priority;
     }
   }
-  return tasks;
+  return updatedTasks;
 }
 app.get("/tasks/edit-priority", (req, res) => {
   let taskId = parseInt(req.query.taskId);
   let priority = parseInt(req.query.priority);
-  let result = editPriorityById(tasks, taskId, priority);
+  let result = editPriorityById(updatedTasks, taskId, priority);
   res.json({ tasks: result });
 });
 // Endpoint 5. Edit/Update Task Text
-function editTextById(tasks, taskId, text) {
-  for (let i = 0; i < tasks.length; i++) {
-    if (tasks[i].taskId === taskId) {
-      tasks[i].text = text;
+let newTasksArray = [
+  { taskId: 1, text: "Fix bug #101", priority: 1 },
+  { taskId: 2, text: "Implement feature #202", priority: 1 },
+  { taskId: 3, text: "Write documentation", priority: 3 },
+  { taskId: 4, text: "Review code", priority: 1 },
+];
+function editTextById(newTasksArray, taskId, text) {
+  for (let i = 0; i < newTasksArray.length; i++) {
+    if (newTasksArray[i].taskId === taskId) {
+      newTasksArray[i].text = text;
     }
   }
-  return tasks;
+  return newTasksArray;
 }
 app.get("/tasks/edit-text", (req, res) => {
   let taskId = parseInt(req.query.taskId);
   let text = req.query.text;
-  let result = editTextById(tasks, taskId, text);
+  let result = editTextById(newTasksArray, taskId, text);
   res.json({ tasks: result });
 });
-// Endpoint 6. Delete a Task from the Task List
-function deleteTasksById(task, taskId) {
+//Endpoint 6. Delete a Task from the Task List
+function deleteTaskById(task, taskId) {
   return task.taskId != taskId;
 }
 app.get("/tasks/delete", (req, res) => {
   let taskId = parseInt(req.query.taskId);
-  let result = tasks.filter((task) => deleteTasksById(task, taskId));
-  res.json({tasks: result });
+  let result = newTasksArray.filter((task) => deleteTaskById(task, taskId));
+  res.json({ tasks: result });
 });
 // Endpoint 7. Filter Tasks by Priority
-function filterTasksByPriority(task, priority) {
+let tasksAfterDeletion = [
+  { taskId: 1, text: "Fix bug #101", priority: 1 },
+  { taskId: 3, text: "Write documentation", priority: 3 },
+  { taskId: 4, text: "Review code", priority: 1 },
+];
+function filterByPriority(task, priority) {
   return task.priority === priority;
 }
 app.get("/tasks/filter-by-priority", (req, res) => {
   let priority = parseInt(req.query.priority);
-  let result = tasks.filter((task) => filterTasksByPriority(task, priority));
+  let result = tasksAfterDeletion.filter((task) => filterByPriority(task, priority));
   res.json({ tasks: result });
 });
